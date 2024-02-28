b18d7e3f0edd7b776e87ce06c913ccf781c34917
efe <113731512+efenatuyo@users.noreply.github.com>
2024-02-28 14:55:47 +0100

    Update __init__.py

--- src/scrape/__init__.py
+++ src/scrape/__init__.py
@@ -4,16 +4,20 @@ def scrape_assets(cookie, keywords, subcategory):
     
     items =  requests.get(f"https://catalog.roblox.com/v1/search/items?category=Clothing&limit=120&salesTypeFilter=1&sortAggregation={random.choice(['1', '3', '5'])}&sortType={random.randint(0, 2)}&subcategory={subcategory}&minPrice=5&keyword={keywords}", 
                         cookies={".ROBLOSECURITY": cookie.cookie}, 
-                        headers={"x-csrf-token": cookie.x_token()}).json()["data"]
-    return [item['id'] for item in items]
+                        headers={"x-csrf-token": cookie.x_token()})
+    if items.status_code == 200:
+        return [item['id'] for item in items.json()["data"]]
+    else:
+        return []
 
 def sort_assets(cookie, ids, blacklisted_creators, blacklisted_words, upload_without_blacklisted_words):
     response = requests.post("https://catalog.roblox.com/v1/catalog/items/details", 
                             json={"items": [{"itemType": "Asset", "id": id} for id in ids]},
                             cookies={".ROBLOSECURITY": cookie.cookie},
-                            headers={"x-csrf-token": cookie.x_token()}).json()["data"]
-    items = []
-    for item in response:
+                            headers={"x-csrf-token": cookie.x_token()})
+    if response.status_code == 200:
+     items = []
+     for item in response.json()["data"]:
         dnd = False
         if item["creatorTargetId"] in blacklisted_creators:
             continue
@@ -30,4 +34,6 @@ def sort_assets(cookie, ids, blacklisted_creators, blacklisted_words, upload_wit
         else:
             item["name"] = item['name'].replace("/", " ")
             items.append(item)
-    return items
+     return items
+    else:
+        return []

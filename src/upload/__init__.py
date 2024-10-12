e6fd770fbeee78e0b830744794eef10388ac1eb9
efe <113731512+efenatuyo@users.noreply.github.com>
2024-10-12 17:37:10 +0200

    Update __init__.py

--- src/upload/__init__.py
+++ src/upload/__init__.py
@@ -1,4 +1,4 @@
-import requests, json, time
+import requests, json, time, uuid
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 def create_asset(name, path, asset_type, cookie, group_id, description, _total_tries, wait_time):
@@ -34,17 +34,29 @@ def create_asset(name, path, asset_type, cookie, group_id, description, _total_t
             total_tries += 1
             time.sleep(wait_time)
 
-def release_asset(cookie, asset_id, price):
+def release_asset(cookie, asset_id, price, name, description, group_id):
     headers = {
         "X-CSRF-TOKEN": cookie.x_token(),
         "Content-Type": "application/json",
         "Cookie": f".ROBLOSECURITY={cookie.cookie};"
     }
-    data = {
-        "saleAvailabilityLocation": [0, 1],
-        "priceConfiguration":
-            {"priceInRobux": price},
-        "saleStatus": "OnSale"
+    data = {    
+        "saleLocationConfiguration": {"saleLocationType": 1, "places": []},
+        "targetId": asset_id,
+        "priceInRobux": price,
+        "publishingType": 2,
+        "idempotencyToken": str(uuid.uuid4()),
+        "publisherUserId": cookie.user_id,
+        "creatorGroupId": group_id,
+        "name": name,
+        "description": description,
+        "isFree": False,
+        "agreedPublishingFee": 0,
+        "priceOffset": 0,
+        "quantity": 0,
+        "quantityLimitPerUser": 0,
+        "resaleRestriction": 2,
+        "targetType": 0
     }
-    return requests.post(f"https://itemconfiguration.roblox.com/v1/assets/{asset_id}/release", headers=headers, json=data)
+    return requests.post(f"https://itemconfiguration.roblox.com/v1/collectibles", headers=headers, json=data)
     

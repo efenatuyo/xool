bd25041a43c768179efc1e1ab0e37964e8809558
efe <113731512+efenatuyo@users.noreply.github.com>
2024-10-12 17:37:01 +0200

    Update __init__.py

--- src/cookie/__init__.py
+++ src/cookie/__init__.py
@@ -5,7 +5,9 @@ class cookie:
         self.cookie = cookie
         self._x_token = None
         self.last_generated_time = 0
+        self.user_id = None
         self.generate_token()
+        self.get_user_id()
 
     def generate_token(self):
         self._x_token = requests.post("https://auth.roblox.com/v2/logout", cookies={".ROBLOSECURITY": self.cookie}).headers.get("x-csrf-token")
@@ -16,3 +18,6 @@ class cookie:
         if current_time - self.last_generated_time >= 120:
             self.generate_token()
         return self._x_token
+    
+    def get_user_id(self):
+        self.user_id = requests.get("https://users.roblox.com/v1/users/authenticated", cookies={".ROBLOSECURITY": self.cookie}).json().get("id")

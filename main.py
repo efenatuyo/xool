fb97a47d4f82922bf426624a9c5df4846a563adc
efe <113731512+efenatuyo@users.noreply.github.com>
2025-03-01 22:27:09 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ class xool:
     current_directory = os.getcwd()
     types = ["classicshirts", "classicpants"]
     def __init__(self, config):
-        src.files.remove_png()
+        if config["delete_all_images_on_restart"]:
+            src.files.remove_png()
+            
         self.config = config
         for group in config["groups"]:
             for cookie in config["groups"][group]["uploader_cookies"]:
@@ -16,7 +18,8 @@ class xool:
      if not cookie:
          raise Exception("Empty cookie")
      cookie = src.cookie.cookie(cookie)
-     while True:
+     dn_stop = True
+     while dn_stop:
       try:
         current_type = random.choice(self.types)
         items = src.scrape.scrape_assets(cookie, self.config["searching_tags"], current_type)
@@ -24,6 +27,9 @@ class xool:
         scraped = src.scrape.sort_assets(cookie, items[:5], self.config["blacklisted_creators"], self.config["blacklisted_words"], self.config["upload_without_blacklisted_words"]
         )
         for item in scraped:
+            if self.config["dupe_check"] and src.files.is_duplicate_file(f"{self.current_directory}/src/assets/{'shirts' if current_type == 'classicshirts' else 'pants'}", f"{item['name']}_{random.randint(0, 100)}.png"):
+                print(f"DUPE: {item['name']}")
+                continue
             path = src.download.save_asset(item["id"], "shirts" if current_type == "classicshirts" else "pants", item["name"], self.config["max_nudity_value"], self.current_directory)
             if not path: 
                 continue
@@ -40,14 +46,20 @@ class xool:
             response = src.upload.release_asset(cookie, item_uploaded['response']['assetId'], self.config["assets_price"], item["name"], self.config["description"], group_id)
             if response.status_code == 200 and response.json()["status"] == 0:
                 print(f"Released item. ID {item_uploaded['response']['assetId']}")
+                if self.config["upload_amount"] > 1:
+                    self.config["upload_amount"] -= 1
+                elif self.config["upload_amount"] == 1:
+                    dn_stop = False
             else:
                 print(f"Failed to release item. ID {item_uploaded['response']['assetId']}")
-            time.sleep(self.config["sleep_each_upload"])
       except Exception as e:
             if str(e) == "403":
-                print("403")
-                raise Exception("Invalid cookie") 
+                print("403: Could mean invalid cookie.")
+                continue
             print(f"ERROR: {traceback.format_exc()}")
+      finally:
+          time.sleep(self.config["sleep_each_upload"])
+      
         
 
 xool(json.loads(open("config.json", "r").read()))
